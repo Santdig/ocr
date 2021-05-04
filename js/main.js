57c8439ee30b270108dcbaf756d6478814016d01
Santosh Dighe <sdighe1405@gmail.com>
2021-05-04 12:35:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@ function initCameraStream() {
 function takeSnapshot() {
   // if you'd like to show the canvas add it to the DOM
 
-  /*var canvas = document.createElement('canvas');*/
+  //var canvas = document.createElement('canvas');
 
   var width = video.videoWidth;
   var height = video.videoHeight;
@@ -281,6 +281,8 @@ function takeSnapshot() {
   // some API's (like Azure Custom Vision) need a blob with image data
   getCanvasBlob(canvas).then(function (blob) {
     // do something with the image blob
+    var jpegFile = canvas.toDataURL("image/jpeg");
+    recognizeFile(jpegFile)
   });
 }
 
@@ -313,4 +315,68 @@ function createClickFeedbackUI() {
       setTimeout(setFalseAgain, timeOut);
     }
   };
+}
+//**************** New functions for extracting text from image ******************
+
+function recognizeFile(file){
+	$("#log").empty();
+  	const corePath = window.navigator.userAgent.indexOf("Edge") > -1
+    ? 'js/tesseract-core.asm.js'
+    : 'js/tesseract-core.wasm.js';
+
+	const worker = new Tesseract.TesseractWorker({
+		corePath,
+	});
+
+	/*worker.recognize(file,
+		$("#langsel").val()
+	)*/
+  worker.recognize(file, 'eng')
+		.progress(function(packet){
+			console.info(packet)
+			//progressUpdate(packet)
+
+		})
+		.then(function(data){
+			console.log(data)
+			progressUpdate({ status: 'done', data: data })
+		})
+}
+
+function progressUpdate(packet){
+	var log = document.getElementById('log');
+
+	if(log.firstChild && log.firstChild.status === packet.status){
+		if('progress' in packet){
+			var progress = log.firstChild.querySelector('progress')
+			progress.value = packet.progress
+		}
+	}else{
+		var line = document.createElement('div');
+		line.status = packet.status;
+		var status = document.createElement('div')
+		status.className = 'status'
+		status.appendChild(document.createTextNode(packet.status))
+		line.appendChild(status)
+
+		/*if('progress' in packet){
+			var progress = document.createElement('progress')
+			progress.value = packet.progress
+			progress.max = 1
+			line.appendChild(progress)
+		}*/
+
+
+		if(packet.status == 'done'){
+			log.innerHTML = ''
+			var pre = document.createElement('pre')
+			pre.appendChild(document.createTextNode(packet.data.text.replace(/\n\s*\n/g, '\n')))
+			line.innerHTML = ''
+			line.appendChild(pre)
+			//$(".fas").removeClass('fa-spinner fa-spin')
+			//$(".fas").addClass('fa-check')
+		}
+
+		log.insertBefore(line, log.firstChild)
+	}
 }
